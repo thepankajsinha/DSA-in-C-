04016be38d068a562125ea63fcf7809bd6352cd0
Pankaj kumar <askpankajsinha@gmail.com>
2024-08-15 17:14:07 +0530

    update

--- 5.Pointers/basicsOfPointer.cpp
+++ 5.Pointers/basicsOfPointer.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(){
+
     int num = 10;
 
     // "&" -> address of operator
@@ -13,18 +14,14 @@ int main(){
 
     
     //here "ptr" is a pointer variable which store the address of int data type variable num
-    int *ptr; //Pointer declaration
+    int *ptr;    //Pointer declaration
     ptr = &num;  //pointer initialization
     cout<<ptr<<endl;
     //output: 0x61ff08
 
-    //how to store the address of another pointer variable
-    int **ptr2 = &ptr;
-    cout<<*ptr2<<endl;
-    //output: 0x61ff08
-
+    
     // "*" -> dereference operator
-    //It is used to access and modify the value which is pointed to by pointer variable
+    //It is used to access and modify the value which is pointed by pointer variable
     *ptr = 50;
     cout << *ptr<<endl;
     //output: 50
@@ -36,6 +33,13 @@ int main(){
     int &b = a; //reference declaration
     cout << a <<endl;
     cout << b <<endl;
+
+
+    //how to store the address of another pointer variable
+    int **ptr2 = &ptr;
+    cout<<*ptr2<<endl;
+    //output: 0x61ff08
+
 
     
 }
