4b55861f02a029a2fa749bf5ce87fbbe8dd6dce3
Pankaj kumar <askpankajsinha@gmail.com>
2024-08-21 17:36:14 +0530

    update

--- 14.Queues/reverseFirstKElements.cpp
+++ 14.Queues/reverseFirstKElements.cpp
@@ -1,4 +1,4 @@
-//Reverse queue using stack
+//Reverse first K elements
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,45 +8,50 @@ void display(queue<int>& q){
         int x = q.front();
         cout<<x<<" ";
         q.pop();
-        q.push(x); // Push the popped element back to the queue
+        q.push(x); 
     }
     cout<<endl;
 }
 
-void reverseQueue(queue<int>& q){
+void reverseFistKElement(queue<int>& q, int k){
     stack<int> st;
-    //move all elements of queue into the stack
-    while(q.size()>0){
+    //move k elements of queue into the stack
+    for (int i = 0; i < k; i++){
         int x = q.front();
         q.pop();
         st.push(x);
     }
     
-    // Move all elements back to the queue
+    // Move k elements back to the queue
     while(st.size()>0){
         int x = st.top();
         st.pop();
         q.push(x);
     }
+
+    // Reverse the remaining elements of the queue
+    for (int i = 0; i < q.size()-k; i++){
+        int x = q.front();
+        q.push(x);
+        q.pop();
+    }
 }
 
 
 int main(){
-
-    // Create a queue
     queue<int> q;
 
-    // Insert elements into the queue
     q.push(1);
     q.push(2);
     q.push(3);
     q.push(4);
     q.push(5);
+    int k = 3;
 
     cout<<"Original Queue: ";
     display(q);
 
-    reverseQueue(q);
+    reverseFistKElement(q,k);
     
     cout<<"Reversed Queue: ";
     display(q);
